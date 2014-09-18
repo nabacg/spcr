341f39149def2290233cd7ecf6510d89f39c4bd9
nabacg <nabacg@gmail.com>
2014-09-18 21:34:46 +0100

    adding numeric conversion for csv parsing

--- src/spcr/parser.clj
+++ src/spcr/parser.clj
@@ -2,6 +2,20 @@
   (:require [clojure.data.csv :as csv]
             [clojure.java.io :as io]))
 
+;;move to new utils namespace
+(defn numeric? [s]
+  (if-let [s (seq s)]
+    (let [s (if (= (first s) \-) (next s) s)
+          s (drop-while #(Character/isDigit %) s)
+          s (if (= (first s) \.) (next s) s)
+          s (drop-while #(Character/isDigit %) s)]
+      (empty? s))))
+
+(defn cast-numerics [str]
+  (if (numeric? str)
+    (Float. str)
+    str))
+
 (defn file->list [path]
   (with-open [input-file (io/reader path)]
     (doall
@@ -9,7 +23,7 @@
 
 (defn list->dicts [[header & body]]
   (let [keys (map keyword header)]
-    (map #(zipmap keys %)
+    (map #(zipmap keys (map cast-numerics %))
          body)))
 
 (defn file->dicts [path]
