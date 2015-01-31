fac4f0a0b473d2e63bdcafb3a90cfe50051c6263
nabacg <nabacg@gmail.com>
2015-01-31 17:26:22 +0000

    filter fn refactor

--- src/cljs/spcr/core.cljs
+++ src/cljs/spcr/core.cljs
@@ -1,7 +1,7 @@
 (ns spcr.core
   (:require [reagent.core :as reagent :refer [atom]]
             [ajax.core :refer [POST GET]]
-            [clojure.string :refer [blank?]]
+            [clojure.string :refer [blank? join]]
             [json-html.core :refer [edn->hiccup]]))
 
 (enable-console-print!)
@@ -13,21 +13,24 @@
                   :data-view []}))
 
 (defn get-data []
-  (GET "/data"
+  (GET "/label"
        {
         :format :json
         :handler (fn [raw-data] (swap! state #(-> %
                                                  (assoc :raw-data raw-data)
                                                  (assoc :data-view raw-data))))}))
+(comment
+  (defn list-items [lst]
+    [:ul.list-group
+     (for [i lst]
+       [:li.list-group-item
+        (str i)
+        (comment  (if (coll? i)
+                    (list-items i)
+                    (str i)))])]))
 
-(defn list-items [lst]
-  [:ul.list-group
-   (for [i lst]
-     [:li.list-group-item
-      (str i)
-      (comment  (if (coll? i)
-                  (list-items i)
-                  (str i)))])])
+(defn list-items [items]
+  [:p (join ", " items)])
 
 (defn table-row [headers row]
   [:tr
@@ -51,25 +54,26 @@
   [:div
    ( draw-table (:data-view @state))])
 
+(defn match-all-columns [filter-str]
+  (fn [row]
+    (some (fn [cell]
+            (re-find
+             (re-pattern filter-str)
+             (str cell)))
+          (vals row))))
+
 (defn handle-filter [filter-str]
   (.log js/console filter-str)
-  (let [filtered-data (if (not (blank? filter-str))
-                        (filter (fn [row]
-                                  (some (fn [val]
-                                          (re-find (re-pattern filter-str) (str val)))
-                                        (vals row)))
-                                (:raw-data @state))
-                        (:raw-data @state))]
+  (let [raw-data (:raw-data @state)
+        filtered-data (if (not (blank? filter-str))
+                        (filter match-all-columns raw-data)
+                        raw-data)]
     (swap! state #(-> %
                       (assoc :filter filter-str)
                       (assoc :data-view filtered-data)))))
 
 (defn home []
   [:div
-   [:div.row
-    [:div.col-md-2]
-    [:div.col-md-10
-     [:h3 "TEST"]]]
    [:div.row
     [:div.col-md-5
      [:input {:type "text"
